55b0eee18d6c96d0f0bce442a01a7e29cb035b6d
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-02-12 15:03:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
-<!DOCTYPE html>
-<html>
-<body onload="loadXMLDoc()">
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://xmldemomt.herokuapp.com//rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("cars");
+?>
 
-<h2>Using the XMLHttpRequest Object</h2>
+ <h1>Tracks</h1>
 
-<div id="demo">
+ <?php
+ foreach( $content as $data )
+ {?>
+     <div class="border">
+     <?php
+     $title = $data->getElementsByTagName("carbrand")->item(0)->nodeValue;
+     $artist = $data->getElementsByTagName("carstatus")->item(0)->nodeValue;
+     $genre = $data->getElementsByTagName("carname")->item(0)->nodeValue;
+     $album = $data->getElementsByTagName("carprice")->item(0)->nodeValue;
+     echo "<ul>
+            <h2>$title</h2>
+              <ul>
+                  <li>Brand: $carbrand </li>
+                  <li>Status: $carstatus </li>
+                  <li>Name: $carname </li>
+                  <li>Price: $carprice </li>
+              </ul>
+          </ul>";
+    ?>
+     </div>
+  <?php
+ }
+?>
 </div>
-
-<script>
-function loadXMLDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "emails.xml", true);
-  xhttp.send();
-}
-</script>
-</body>
-</html>
+</div>
